f58d1d235f1cefa8a51b2b202d60f4e29df638d4
O-Isaac <zaragozamendozaisaac@outlook.es>
2025-11-24 09:51:39 +0100

    feat: Update model & add demo

--- models/models.py
+++ models/models.py
@@ -4,6 +4,10 @@ class Libro(models.Model):
     _name = 'library.libro'
     _description = 'Modelo del libro en el estante'
 
-    name = fields.Char()
-    value = fields.Integer()
-    description = fields.Text()
+    title = fields.Char(string="Título del libro", required=True, help="Título del libro")
+    description = fields.Text(string="Sipnosis corta del libro")
+    pages = fields.Integer(string="Paginas del libro", required=True, default=0)
+    price = fields.Float(string="Precio del libro", digits=(10, 2), required=True)
+    is_avalible = fields.Boolean(string="¿Disponible?", default=False)
+    public_date = fields.Date(string="Fecha de publicacion")
+    created_timestamp = fields.Datetime(string="Fecha de creacion", default=fields.Datetime.now)
